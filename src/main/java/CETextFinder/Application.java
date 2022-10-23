6988274604ff7690a19c25a676ae22add96a0f93
Jose Maria Vindas Ortiz <josemaria130@outlook.es>
2022-10-22 22:49:57 -0600

    Implementacion Basica de Socket

--- src/main/java/CETextFinder/Application.java
+++ src/main/java/CETextFinder/Application.java
@@ -3,8 +3,49 @@ package CETextFinder;
 import CETextFinder.EstructurasDeDatos.ArbolBinario.AVLBinaryTree;
 import CETextFinder.EstructurasDeDatos.ArbolBinario.BinaryTree;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
+
 public class Application {
     public static void main(String[] args){
+        new Application();
+    }
+    public Application() {
+        try {
+            /* Se crea el socket cliente */
+            Socket socket = new Socket("localhost", 25557);
+            System.out.println("conectado");
 
+            /* Se hace que el cierre espere a la recogida de todos los datos desde
+             * el otro lado */
+            socket.setSoLinger(true, 10);
+
+            /* Se obtiene un stream de lectura para leer objetos */
+            DataInputStream bufferEntrada =
+                    new DataInputStream(socket.getInputStream());
+
+            /* Se lee un Datosocket que nos envía el servidor y se muestra
+             * en pantalla */
+            DatoSocket dato = new DatoSocket("");
+            dato.readObject(bufferEntrada);
+            System.out.println("Cliente Java: Recibido " + dato.toString());
+
+            /* Se obtiene un flujo de envio de datos para enviar un dato al servidor */
+            DataOutputStream bufferSalida =
+                    new DataOutputStream(socket.getOutputStream());
+
+            /* Se crea el dato y se escribe en el flujo de salida */
+            DatoSocket aux = new DatoSocket("Adios");
+            aux.writeObject(bufferSalida);
+
+            System.out.println("Cliente Java: Enviado " + aux.toString());
+
+             /* La llamada a setSoLinger() hará que el cierre espere a que el otro
+             lado retire los datos que hemos enviado */
+            socket.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
