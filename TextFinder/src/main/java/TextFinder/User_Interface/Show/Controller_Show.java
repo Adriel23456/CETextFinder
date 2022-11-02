b39e88b51b3452ce30c4cc388620fb1141b1f845
Adriel23456 <adriel.chaves@hotmail.com>
2022-11-02 01:29:13 -0600

    Version Final 1.2.3

--- TextFinder/src/main/java/TextFinder/User_Interface/Show/Controller_Show.java
+++ TextFinder/src/main/java/TextFinder/User_Interface/Show/Controller_Show.java
@@ -3,6 +3,7 @@ package TextFinder.User_Interface.Show;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
 import java.io.File;
 import java.io.IOException;
 
@@ -26,18 +27,71 @@ public class Controller_Show {
         if (model.current_document.getType().equals("txt")){
             Desktop desktop = Desktop.getDesktop();
             try {
-                System.out.println("Se abrió un documento TXT");
                 desktop.open(file);
+                String text = model.current_document.getText1();
+                String[] words = text.split("[ \\n(/)\"\t\\t\n,?.!]+");
+                int p;
+                for (p = 0; p < words.length; p++) {
+                    if (p == model.getCurrent_document().getPosiciones().getElement(0)) {
+                        break;
+                    }
+                }
+                String Palabra = words[p];
+                char[] CharPalabra = Palabra.toCharArray();
+                Robot robot = new Robot();
+                robot.delay(1000);
+                robot.mouseMove(1000, 300);
+                robot.keyPress(KeyEvent.VK_CONTROL);
+                robot.keyPress(KeyEvent.VK_B);
+                robot.keyRelease(KeyEvent.VK_CONTROL);
+                robot.keyRelease(KeyEvent.VK_B);
+                for (char Letra : CharPalabra) {
+                    robot.keyPress(Character.toUpperCase(Letra));
+                    robot.keyRelease(Character.toUpperCase(Letra));
+                }
+                robot.keyPress(KeyEvent.VK_ENTER);
+                robot.keyRelease(KeyEvent.VK_ENTER);
+                robot.keyPress(KeyEvent.VK_ESCAPE);
+                robot.keyRelease(KeyEvent.VK_ESCAPE);
             } catch (IOException ex) {
                 throw new RuntimeException(ex);
+            } catch (AWTException e) {
+                throw new RuntimeException(e);
             }
-        } else if (model.current_document.getType().equals("docx")) {
+        }
+        else if (model.current_document.getType().equals("docx")) {
             Desktop desktop = Desktop.getDesktop();
             try {
-                System.out.println("Se abrió un documento DOCX");
                 desktop.open(file);
+                String text = model.current_document.getText1();
+                String[] words = text.split("[ \\n(/)\"\t\\t\n,?.!]+");
+                int p;
+                for (p = 0; p < words.length; p++) {
+                    if (p == model.getCurrent_document().getPosiciones().getElement(0)) {
+                        break;
+                    }
+                }
+                String Palabra = words[p];
+                char[] CharPalabra = Palabra.toCharArray();
+                Robot robot = new Robot();
+                robot.delay(1500);
+                robot.mouseMove(1000, 300);
+                robot.keyPress(KeyEvent.VK_CONTROL);
+                robot.keyPress(KeyEvent.VK_B);
+                robot.keyRelease(KeyEvent.VK_CONTROL);
+                robot.keyRelease(KeyEvent.VK_B);
+                for (char Letra : CharPalabra) {
+                    robot.keyPress(Character.toUpperCase(Letra));
+                    robot.keyRelease(Character.toUpperCase(Letra));
+                }
+                robot.keyPress(KeyEvent.VK_ENTER);
+                robot.keyRelease(KeyEvent.VK_ENTER);
+                robot.keyPress(KeyEvent.VK_ESCAPE);
+                robot.keyRelease(KeyEvent.VK_ESCAPE);
             } catch (IOException ex) {
                 throw new RuntimeException(ex);
+            } catch (AWTException e) {
+                throw new RuntimeException(e);
             }
         }
         else{
@@ -45,8 +99,34 @@ public class Controller_Show {
             try {
                 System.out.println("Se abrió un documento PDF");
                 desktop.open(file);
+                String text = model.current_document.getText1();
+                String[] words = text.split("[ \\n(/)\"\t\\t\n,?.!]+");
+                int p;
+                for (p = 0; p < words.length; p++) {
+                    if (p == model.getCurrent_document().getPosiciones().getElement(0)) {
+                        break;
+                    }
+                }
+                System.out.println(words[p]);
+                String Palabra = words[p];
+                char[] CharPalabra = Palabra.toCharArray();
+                Robot robot = new Robot();
+                robot.delay(2000);
+                robot.mouseMove(1000, 300);
+                robot.keyPress(KeyEvent.VK_CONTROL);
+                robot.keyPress(KeyEvent.VK_F);
+                robot.keyRelease(KeyEvent.VK_CONTROL);
+                robot.keyRelease(KeyEvent.VK_F);
+                for (char Letra : CharPalabra) {
+                    robot.keyPress(Character.toUpperCase(Letra));
+                    robot.keyRelease(Character.toUpperCase(Letra));
+                }
+                robot.keyPress(KeyEvent.VK_ENTER);
+                robot.keyRelease(KeyEvent.VK_ENTER);
             } catch (IOException ex) {
                 throw new RuntimeException(ex);
+            } catch (AWTException e) {
+                throw new RuntimeException(e);
             }
         }
     }
